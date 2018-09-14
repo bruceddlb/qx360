95cc4fc1d33892c2fee43c9c94a6f52601bb3aac
bruceddlb <bruced@126.com>
2018-09-14 16:52:56 +0800

    update notice

---
+++ init.bat
@@ -0,0 +1,8 @@
+echo "# rchl" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/bruceddlb/qx360.git
+git pull origin master
+git push -u origin master
---
+++ update-git.bat
@@ -0,0 +1,5 @@
+git status
+git add *
+git commit -m "update notice"
+git pull
+git push origin master
